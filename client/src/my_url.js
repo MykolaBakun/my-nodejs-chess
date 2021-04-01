60cfc16814aafa18121e0453990561937f8912b6
Mykola Bakun <36553905+MykolaBakun@users.noreply.github.com>
2021-04-01 18:16:47 +0300

    Save url

---
+++ client/src/my_url.js
@@ -0,0 +1,1 @@
+process.env.API_URL = "http://test_from_file/"
--- scripts/replace_api_url.sh
+++ scripts/replace_api_url.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-
-find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,API_URL,'"$API_URL"',g' {} \;
+# find '/usr/share/nginx/html' -name '*.js' -exec sed -i -e 's,API_URL,'"$API_URL"',g' {} \;
+echo "process.env.API_URL = \"http://test_from_file/"\" > /nodejs-app/client/src/my_url.js;
 nginx -g "daemon off;"
